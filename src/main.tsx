366aff68008d0cfdb404e5eef4a32a64e5ab2cdc
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-05-29 02:16:59 -0300

    configuração das rotas na main e exclusão dos arquivos app.tsx e app.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './global.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Capa } from './routes/Capa';
+import { ErrorNotFound } from './components/ErrorNotFound';
+import ToDo from './routes/ToDo';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Capa />
+    ),
+    errorElement: (
+      <ErrorNotFound />
+    ),
+  },
+  {
+    path: "/to-do",
+    element: (
+      <ToDo />
+    ),
+    errorElement: (
+      <ErrorNotFound />
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
